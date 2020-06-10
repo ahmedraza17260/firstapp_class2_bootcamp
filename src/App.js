9120cbea9179ea1ee282bd27de0f12f42dd9bcbb
ahmedraza17260 <ahmedraza17260@gmail.com>
2020-06-10 19:50:28 +0500

    remove logo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
     return ( <
         div >
         <
-        h1 > Hello World Class 2 BootCamp < /h1> <
-        /div>
+        h1 > Hello World Class 2 BootCamp < /h1> < /
+        div >
     );
 }
 
